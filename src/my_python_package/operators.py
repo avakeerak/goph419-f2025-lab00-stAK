5f67984d78712a0be013ead0a28e800053953d9b
avakeerak <ava.keerak@ucalgary.ca>
2025-09-09 15:41:38 -0600

    updated operators.py

--- src/my_python_package/operators.py
+++ src/my_python_package/operators.py
@@ -27,3 +27,33 @@ def multiply(x, y):
     The (element-wise) product of x and y.
     """
     return x * y
+
+def divide(x, y):
+    """"Divide two numbers or arrays.
+    Parameters
+    ----------
+    x : int or float or array_like
+    The numerator.
+    y : int or float or array_like
+    The denomenator.
+    Returns
+    -------
+    int or float or array_like
+    The (element-wise) quotient of x and y.
+    """
+    return x / y 
+
+def exp(x, y):
+    """Raise the base to the power of the exponent.
+    Parameters
+    ----------
+    x : int or float
+    The base.
+    y : int or float
+    The exponent.
+    Returns
+    -------
+    int or float
+    The (element-wise) power of x and y.
+    """
+    return x ** y
